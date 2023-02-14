56f3c4faeb3e9dca716d0764e995ec85dbd1c4c4
DaniloG <dan_gaaspar@hotmail.com>
2023-02-13 22:31:24 -0300

    Adicionando alert

---
+++ app.js
@@ -0,0 +1,5 @@
+var userName = prompt('Olá,qual seu nome?')
+var userAge = prompt('Qual a sua idade?')
+const welcomeMessage = 'Olá'+ ' ' + userName + ' ' + 'sua idade é' + ' ' + userAge
+
+alert(welcomeMessage)
